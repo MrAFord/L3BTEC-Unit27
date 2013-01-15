b24dead5f5b8052d9e121b7261332ec74e84fff4
Mr Ford <aford@theoxfordacademy.org.uk>
2013-01-15 15:26:41 -0800

    added new examples

--- examples/SimplePHP.php
+++ examples/SimplePHP.php
@@ -25,7 +25,16 @@
       </div>
       <div class="row-fluid">
         <div class="span3">
-          <div class="well">This page has examples of basic PHP scripting.</div>
+          <div class="well">This page has examples of basic PHP scripting. You should compare the PHP scipting code in the PHP file to that
+          shown in the browser (you can use <strong>show source</strong> in your browser).
+          <br/>
+                    <br/>
+            Try changing the values of $firstnumber, $secondnumber, $quickanimal and $lazyanimal and see what happens when you reload the page.
+          <br/>
+                    <br/>
+            
+            For background details see the <a href="http://php.net/manual/en/langref.php">PHP Language Reference</a>.
+          </div>
         </div>
         <div class="span9">
 		
@@ -38,12 +47,12 @@
 				$lazyanimal = "dog";
 				
 				// now we can output some HTML like this...
-				echo "<p>The quick brown ", $quickanimal, " jumped over the lazy ", $lazyanimal, </p>;
+				echo "<p>The quick brown ", $quickanimal, " jumped over the lazy ", $lazyanimal, ".</p>";
 						
 			?>
 			
 			<!-- now let's output almost the same thing using HTML -->
-			<p>The quick brown <?php echo $lazyanimal ?> jumped over the lazy <?php echo $quickanimal ?> .</p>
+			<p>The quick brown <?php echo $lazyanimal ?> jumped over the lazy <?php echo $quickanimal ?>.</p>
 			
 			<h2>Operations</h2>
 			
@@ -54,7 +63,47 @@
 			
 			?>
 			<p>Mr. Ford's most favourite number is, of course, <?php echo $firstnumber + $secondnumber ?>.</p>
+            <p><?php echo $firstnumber ?> times <?php echo $secondnumber ?> is equal to <?php echo ($firstnumber * $secondnumber); ?>.</p> 
 		
+            <h2>Looping</h2>
+            
+            <p>Looping = doing things more than once. Sometimes also called <em>iteration</em></p>
+			
+			<?php
+            
+                $smallernumber = $firstnumber / $secondnumber;
+                
+                // QUESTION: where does the number dislayed in the browser start and stop. Why?
+			
+			    for($i=0;$i<$smallernumber;$i++) {
+    		        echo "<p>The number is " . $i . "</p>"; 
+                 
+			    }
+            
+            
+			?>
+            
+            
+            <h2>Conditional</h2>
+    		
+            <p>Conditional = only doing something when something else is true. Sometimes also called <em>branching</em> because the branch you take 
+            depends on something.</p>
+            
+			<?php
+            
+                $smallernumber = $firstnumber / $secondnumber;
+                
+               
+			
+			    if($smallernumber > 10) {
+    		        echo "<p>The small number isn't small enough.</p>"; 
+                }
+            
+            
+			?>
+		
+		
+        
         </div>
         <!-- /span 9 -->
       </div>
