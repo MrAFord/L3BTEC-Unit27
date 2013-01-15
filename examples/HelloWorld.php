60aeb86c28a194c217becb804fd5f2eae0e753e4
Mr Ford <aford@theoxfordacademy.org.uk>
2013-01-15 11:25:22 +0000

    Improved descriptive comments

--- examples/HelloWorld.php
+++ examples/HelloWorld.php
@@ -1,21 +1,43 @@
 <html>
 
-<head></head>
+<!-- this is a PHP file - look at the .php extension! - but we
+	can mix HTML and PHP content.
+	Look carefully and you will see...
+	
+	* Regular HTML tags like <p> for a paragraph
+	
+	* A special tag that denotes PHP code like this...
+	
+	  <!php
+	  
+		// ... php code goes here
+	  
+	  ?>
+	
+	* The PHP code creating part of the web page using echo
+	
+	-->
+
+<head><title>PHP Hello World</title></head>
 
 <body>
 
-This is plain boring HTML with...
+	<!-- start with regular HTML -->
+	<h1>PHP Hello World Example</h1>
+	
+	<p>In this example we're mixing	plain boring HTML with...</p>
 
-<br />
+	
+	<!-- now include PHP code to write something out -->
+	<?php
+		// this is a comment in the PHP code
 
-<?php
+		echo "<p>...some cool PHP to say: <b>Hello, World</b></p>";
 
+	?>
 
-echo "...some cool PHP saying <b>Hello, World</b>";
-
-?>
-
-
+	<!-- and now back to regular HTML -->
+	<p>Compare the source of this PHP file with the HTML source in the browser (use <strong>view source</strong> in the browser).</p>
 
 </body>
 
